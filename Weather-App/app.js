d27e4d16dc17052e28f636803b972cffcb39d552
Alyssa Pratt <alyssakpratt@gmail.com>
2020-08-18 17:21:35 -0500

    receives  API and fetches data

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -4,7 +4,21 @@ window.addEventListener('load', ()=> {
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
-            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `http://api.weatherapi.com/v1/current.json?key=f1fa4ac4a07e48658f5220351201808&q=${lat},${long}`;
+
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data)
+                });
+
+        
         });
     }
 });
+
